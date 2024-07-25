27ac9f0081f56254b1c92545836def297cce22d0
Leejoowon123 <101091485+Leejoowon123@users.noreply.github.com>
2024-07-25 10:30:14 +0900

    crawling_Modularization

---
+++ send_to_mysql.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+
+def send_2_mysql(p_df, p_company):
+    # MySQL 데이터베이스 연결 정보 설정
+    user = 'root'
+    password = 'root1234'
+    host = 'localhost'
+    database = 'dbname'
+
+    # SQLAlchemy 사용
+    engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
+
+    # 데이터프레임을 MySQL 테이블로 저장
+    table_name = p_company + '_faq_data'
+    p_df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
